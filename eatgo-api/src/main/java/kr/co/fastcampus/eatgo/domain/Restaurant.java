4ca24e89cdb503083e0606960510584106b59824
김도균 <dkkop225@gamil.com>
2020-03-06 00:23:22 +0900

    day3

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -3,8 +3,11 @@ package kr.co.fastcampus.eatgo.domain;
 public class Restaurant {
     private final String name;
     private final String address;
+    private final Long id;
 
-    public Restaurant(String name, String address) {
+
+    public Restaurant(Long id, String name, String address) {
+        this.id = id;
         this.name = name;
         this.address = address;
     }
@@ -20,5 +23,9 @@ public class Restaurant {
 
     public String getInformation() {
         return name +" in " + address;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -9,18 +9,19 @@ import static org.hamcrest.core.Is.is;
 
 public class RestaurantTests {
 
-        @Test
-        public void creation() {
-            Restaurant restaurant = new Restaurant("Bob Zip","Seoul");
-            assertThat(restaurant.getName() , is("Bob Zip")); //단정문
-            assertThat(restaurant.getAddress(), is("Seoul"));
-        }
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant(1004L,"Bob Zip","Seoul");
+        assertThat(restaurant.getId(),is(1004L));
+        assertThat(restaurant.getName() , is("Bob Zip")); //단정문
+        assertThat(restaurant.getAddress(), is("Seoul"));
+    }
 
-        @Test
-        public void information() {
-            Restaurant restaurant = new Restaurant("Bob Zip","Seoul");
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant(1004L,"Bob Zip","Seoul");
 
-            assertThat(restaurant.getInformation(),is("Bob Zip in Seoul"));
-        }
+        assertThat(restaurant.getInformation(),is("Bob Zip in Seoul"));
+    }
 
 }
